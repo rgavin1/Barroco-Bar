f01632a9feca532ed86baa1a09ad9c14f24777a6
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-11-16 23:27:49 -0500

    Init Commit

---
+++ react-app-barroco/src/App.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="grid-layout">
+      <header>
+		<div class="bar-tapp-wrapper">
+			<span class="header-logo"></span>
+			<span class="arepaBar_ConcertCafe">Arepa Bar + Concert Cafe</span>
+      </div>
+      </header>
+    </div>
+    <nav>
+    <ul class="nav-container">
+      <li class="container-content"><a href="#">Menu</a></li>
+      <li class="container-content"><a href="#">Order Online</a></li>
+      <li class="container-content"><a href="#">About Us</a></li>
+      <li class="container-content"><a href="#">Happy Hours</a></li>
+      <li class="container-content"><a href="#">Locations</a></li>
+      <li class="container-content"><a href="#">Live Music</a></li>
+      <li class="container-content"><a href="#">Contact</a></li>
+      </ul>
+  </nav>
+  <div id="social-media">
+	<ul class="social-media-wrapper">
+	<li class="fb">
+    <a href="#">
+      <img src="https://static.wixstatic.com/media/0fdef751204647a3bbd7eaa2827ed4f9.png/v1/fill/w_86,h_86,al_c,usm_0.66_1.00_0.01/0fdef751204647a3bbd7eaa2827ed4f9.png
+	" alt="facebook logo" />
+    </a>
+  </li>
+	<li class="ig">
+    <a href="#">
+      <img src="https://static.wixstatic.com/media/01c3aff52f2a4dffa526d7a9843d46ea.png/v1/fill/w_86,h_86,al_c,usm_0.66_1.00_0.01/01c3aff52f2a4dffa526d7a9843d46ea.png" alt="Instagram logo" />
+    </a>
+  </li>
+	</ul>
+</div>
+<section id="msg-wrapper">
+		<ul class="msg">
+			<li>Birdtown (Lakewood, OH) - Larchmere (Cleveland, OH) - Crocker Park (coming soon)</li>
+			<li>| Open daily at 11am |</li>
+			<li>No Reservations</li>
+		</ul>
+	</section>
+  <main>
+	<section class="section1">
+		<h2 class="section1-header">Nuestra Cultura Barroca</h2>
+		<p class="section1-para">When you walk into Barroco our family will ensure that your experience is amped to the max. Our eclectic and vibrant atmosphere will have you and your guests thinking you are in another world. Our award winning menu will have you bragging to your friends and begging for the next visit.</p>
+	</section>
+	<section class="hero-image-video">
+	<video autoplay muted loop class="myVideo" poster="https://static.wixstatic.com/media/cb2e86_a1af32d37aea4588a5a9c2ae8430c5d3f000.jpg/v1/fill/w_720,h_383,al_c,q_80,usm_0.33_1.00_0.00/cb2e86_a1af32d37aea4588a5a9c2ae8430c5d3f000.jpg"><source src="https://video.wixstatic.com/video/cb2e86_a1af32d37aea4588a5a9c2ae8430c5d3/480p/mp4/file.mp4" /></video>
+	<img class="barroco-size" src="https://static.wixstatic.com/media/cb2e86_fd0bc32b806a49a396492eed7e36cd24~mv2.png/v1/fill/w_1200,h_542,al_c,usm_0.66_1.00_0.01/cb2e86_fd0bc32b806a49a396492eed7e36cd24~mv2.png" alt="" />		
+		</section>
+	<section>
+		<ul class="specials">
+			<li>
+				<h3 class="hh_heading1">Happy Hours</h3>
+					<ul class="hh_menu1">
+						<li>MONDAYS - $5 MOJITOS</li>
+						<li>TUESDAYS - $5 SANGRIAS,  1/2 PRICED WINE BOTTLES</li>
+						<li>WEDNESDAYS - $5 MARGARITAS</li>
+						<li>THURSDAYS - $5 PIÑA COLADAS</li>
+					</ul>
+			</li>
+		</ul>
+			<div class="fancy_drink"></div>
+		</section>
+		<section>
+			<div class="fancy_drink2">			
+					</div>
+				<ul class="hh_menu2">
+					<h4>MON-FRI 3PM-6PM</h4>
+					<li>$5 COCKTAILS</li>
+					<li>$4 DRAFTS</li>
+					<li>1/2 PRICED APPETIZERS</li>
+					<li>20% OFF LARGE GROUP APPETIZERS</li>
+			<li>Our cocktails are all handmade, with mixes and fresh juices made in house.</li>
+			</ul>
+</section>
+	<section class="loc-wrapper">
+		<h2 class="location-heading">LOCATIONS</h2>
+		<ul class="locations-container">
+			<li class="cir">
+				<h3><span class="heading1">Barroco</span> in Birdtown</h3>
+				<p>12906 MADISON AVE<br/>
+				LAKEWOOD, OH 44107<br/>
+				216-221-8127</p>
+			<img class="cir" src="https://static.wixstatic.com/media/cb2e86_366d631e05ff463bacb0e51c866eb3cf~mv2.jpg/v1/fill/w_757,h_606,al_l,q_85/cb2e86_366d631e05ff463bacb0e51c866eb3cf~mv2.jpg" />
+			</li>
+			<li class="cir">
+				<h3><span class="heading1">Barroco</span> in Larchmere</h3>
+				<p>12718 LARCHMERE BLVD<br/>
+				CLEVELAND, OH 44120<br/>
+				216-938-9301</p>
+				<img class="cir" src="https://static.wixstatic.com/media/cb2e86_de7a586781164a2fb05e923c0f3da2b0~mv2.jpg/v1/fill/w_976,h_778,al_c,q_85,usm_0.66_1.00_0.01/cb2e86_de7a586781164a2fb05e923c0f3da2b0~mv2.jpg" />
+			</li>
+			<li>
+				<h3><span class="heading1">Barroco</span> in Crocker Park (coming soon)</h3>
+				<p>287 CROCKER PARK BLVD<br/>
+				WESTLAKE, OH 44145<br/>
+				COMING SOON</p>
+				<img class="cir" src="https://static.wixstatic.com/media/cb2e86_4a123699e56142f9a271e2118987f9a9~mv2_d_4032_3024_s_4_2.jpg/v1/fill/w_976,h_778,al_c,q_85,usm_0.66_1.00_0.01/cb2e86_4a123699e56142f9a271e2118987f9a9~mv2_d_4032_3024_s_4_2.jpg" />
+			</li>
+		</ul>
+	</section>
+	<section class="live-music">
+			<h2>LIVE MUSIC</h2>
+				<ul>
+					<li>Lakewood: EVERY TUESDAY &  SATURDAY at 7pm</li>
+					<li>Larchmere: EVERY MONDAY & FRIDAY at 7pm</li>
+					<li>Crocker Park: COMING SOON</li>
+				</ul>
+			<img src="https://static.wixstatic.com/media/cb2e86_0254f683ccd24e898459a0ad6a1766f6~mv2_d_2048_1356_s_2.jpg/v1/fill/w_2048,h_1169,al_c,q_90/cb2e86_0254f683ccd24e898459a0ad6a1766f6~mv2_d_2048_1356_s_2.jpg" />
+</section>
+</main>
+<footer class="footer-wrapper">
+	<ul class="footer-content">
+		<li>
+		<h2><span class="barroco">Barroco</span> AREPA BAR & CONCERT CAFE</h2>
+		<h3>WORK AT BARROCO</h3>
+		<p>Send a private message through Facebook with resume</p>
+			</li>
+		<li>
+		<h3>VISIT US</h3>
+		<ul class="visit-us">
+			<li>
+				<h4>BARROCO LAKEWOOD</h4>
+				<p>12906 Madison Ave<br/>
+		Lakewod, OH 44107<br/>
+					Tel: 216-221-8127</p>
+			</li>
+			<li>
+				<h4>BARROCO LARCHMERE</h4>
+		<p>12718 Larchmere Blvd. <br/>
+		Cleveland, OH 44120 <br/>
+			Tel: 216-938-9301</p>
+			</li>
+			<li>
+		<h4>BARROCO CROCKER PARK
+			(COMING SOON)</h4>
+		<p>287 Crocker Park Blvd. <br/>
+			Westlake, OH 44145</p>
+			</li>
+		</ul>
+			</li>
+	</ul>
+	<div>
+		© 2018 By Barroco
+	</div>
+</footer>
+  </div>
+  );
+}
+
+export default App;
